6defc72a6d0ca81ed4c723dcbb80a83e1b9aa7ee
Nestorpq <1208003@senati.pe>
2023-04-05 17:12:37 -0500

    creamos el método listar platos

---
+++ models/Platos.php
@@ -0,0 +1,32 @@
+<?php
+require_once "../models/Conexion.php";
+
+class Curso extends Conexion{
+    private $accesoBD;
+
+
+    public function __CONSTRUCT(){
+        //  almacenamos la conexion obtenida
+        $this->accesoBD = parent::getConexion();
+    }
+
+
+    //  Método listar platos
+    public function listarPlatos(){
+        try {
+            //  1. Preparamos la consulta
+            $consulta = $this->accesoBD->prepare("CALL spu_platos_listar()");
+
+            //  2. Ejecutamos la consulta
+            $consulta->execute();
+
+            //  3. Devolvemos el resultado
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+}
+
+?>
